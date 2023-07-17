4580ccd0437714d47def073842a296115d6b5b7d
stackrev <farbitherast5888@gmail.com>
2023-07-17 05:49:58 +0200

    generate merkle tree json

--- backend/scripts/MerkleTree.js
+++ backend/scripts/MerkleTree.js
@@ -1,5 +1,6 @@
 const keccak256 = require("keccak256");
 const { default: MerkleTree } = require("merkletreejs");
+const fs = require("fs");
 
 const address = [
   "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
@@ -20,3 +21,38 @@ const buf2Hex = (x) => "0x" + x.toString("hex");
 
 // Get Root of Merkle Tree
 console.log(`Here is Root Hash: ${buf2Hex(tree.getRoot())}`);
+
+let data = [];
+
+// Pushing all the proof and leaf in data array
+address.forEach((address) => {
+  const leaf = keccak256(address);
+
+  const proof = tree.getProof(leaf);
+
+  let tempData = [];
+
+  proof.map((x) => tempData.push(buf2Hex(x.data)));
+
+  data.push({
+    address: address,
+    leaf: buf2Hex(leaf),
+    proof: tempData,
+  });
+});
+
+// Create WhiteList Object to write JSON file
+
+let whiteList = {
+  whiteList: data,
+};
+
+//  Stringify whiteList object and formating
+const metadata = JSON.stringify(whiteList, null, 2);
+
+// Write whiteList.json file in root dir
+fs.writeFile(`whiteList.json`, metadata, (err) => {
+  if (err) {
+    throw err;
+  }
+});
