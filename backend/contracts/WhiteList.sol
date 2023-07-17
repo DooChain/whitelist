18256e4f89bb4495c96927926977e35a508786e7
stackrev <farbitherast5888@gmail.com>
2023-07-17 14:50:08 +0200

    contract completed

--- backend/contracts/WhiteList.sol
+++ backend/contracts/WhiteList.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "hardhat/console.sol";
 
 contract WhiteList {
     bytes32 public rootHash;
-    uint256 public count;
+
+    // uint256 public count;
 
     constructor(bytes32 _rootHash) {
         rootHash = _rootHash;
     }
 
-    modifier isWhiteListedAddress(bytes32[] calldata proof) {
-        require(
-            isValidProof(proof, keccak256(abi.encodePacked(msg.sender))),
-            "Not WhiteListed Address"
-        );
-        _;
-    }
+    // modifier isWhiteListedAddress(bytes32[] calldata proof) {
+    //     require(
+    //         isValidProof(proof, keccak256(abi.encodePacked(msg.sender))),
+    //         "Not WhiteListed Address"
+    //     );
+    //     _;
+    // }
 
     function isValidProof(
         bytes32[] calldata proof,
@@ -25,9 +27,13 @@ contract WhiteList {
         return MerkleProof.verify(proof, rootHash, leaf);
     }
 
-    function whiteListIncrement(
-        bytes32[] calldata proof
-    ) external isWhiteListedAddress(proof) {
-        count++;
+    // function whiteListIncrement(
+    //     bytes32[] calldata proof
+    // ) external isWhiteListedAddress(proof) {
+    //     count++;
+    // }
+
+    function checkIfValid(bytes32[] calldata proof) public view returns (bool) {
+        return isValidProof(proof, keccak256(abi.encodePacked(msg.sender)));
     }
 }
--- backend/scripts/run.js
+++ backend/scripts/run.js
@@ -1,9 +1,35 @@
+const fs = require("fs");
+
+let whiteListData = [];
+// Read the contents of the JSON file
+fs.readFile("whiteList.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+
+  try {
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+
+    // Access the whiteList array
+    whiteListData = jsonData.whiteList;
+  } catch (error) {
+    console.log("Error parsing JSON:", error);
+  }
+});
 const main = async () => {
-  const whiteList = await hre.ethers.deployContract("WhiteList", [
-    "0x16b2724f8e59755970a64bf4603da895fe95caae0d23835859efb525b0b597a4",
+  const [owner, _] = await hre.ethers.getSigners();
+  console.log(owner.address);
+
+  const whiteListContract = await hre.ethers.deployContract("WhiteList", [
+    "0x9f8f5a2556959716e288f3c87b99bdd6f65d5e325e75592494d867b245db523a",
   ]);
-  await whiteList.waitForDeployment();
-  console.log(`WhiteList deployed to ${whiteList.target}`);
+  await whiteListContract.waitForDeployment();
+  console.log(`WhiteList deployed to ${whiteListContract.target}`);
+
+  const result = await whiteListContract.checkIfValid(whiteListData[3].proof);
+  console.log(result);
 };
 
 const runMain = async () => {
